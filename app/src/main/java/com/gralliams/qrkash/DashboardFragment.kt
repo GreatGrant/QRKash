87a5ab640834643bf1fa779a59bebcf072e8cd59
Great Grant <greatgrant116@gmail.com>
2023-07-14 19:39:09 +0100

    Add topop dialog

--- app/src/main/java/com/gralliams/qrkash/DashboardFragment.kt
+++ app/src/main/java/com/gralliams/qrkash/DashboardFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.gralliams.qrkash.databinding.FragmentDashBoardBinding
 
@@ -29,7 +30,31 @@ class DashboardFragment : Fragment() {
             username.text = "User"
         }
 
+        binding.btnTopup.setOnClickListener {
+            showTopupOptionsDialog()
+        }
+
         return binding.root
+    }
+
+    private fun showTopupOptionsDialog() {
+        val options = arrayOf("Use QR code", "Use bank transfer")
+
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Topup Options")
+            .setItems(options) { _, index ->
+                when (index) {
+                    0 -> {
+                        // Handle "Use QR code" option
+                        // TODO: Implement the action for using QR code
+                    }
+                    1 -> {
+                        // Handle "Use bank transfer" option
+                        // TODO: Implement the action for using bank transfer
+                    }
+                }
+            }
+            .show()
     }
 
 }
