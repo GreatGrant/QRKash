0e68770cca8b4060c314c469cdfc44e14c32e42d
Great Grant <greatgrant116@gmail.com>
2023-07-16 22:05:49 +0100

    add get virtual account request in Repository

--- app/src/main/java/com/gralliams/qrkash/repository/VirtualAccountRepository.kt
+++ app/src/main/java/com/gralliams/qrkash/repository/VirtualAccountRepository.kt
@@ -8,6 +8,9 @@ class VirtualAccountRepository(private val apiService: FlutterwaveApiService) {
     suspend fun createVirtualAccount(requestBody: VirtualAccountRequest): VirtualAccountResponse {
         return apiService.createVirtualAccount(requestBody)
     }
-  
+
+    suspend fun getVirtualAccount(orderRef: String): VirtualAccountResponse{
+        return apiService.getVirtualAccount(orderRef)
+    }
 }
 
