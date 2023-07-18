5dff9799e0e6e527b3670f8f3f3afe5e4381791b
Great Grant <greatgrant116@gmail.com>
2023-07-18 11:14:42 +0100

    create wallet viewmodel

---
+++ app/src/main/java/com/gralliams/qrkash/viewmodel/WalletViewModel.kt
@@ -0,0 +1,47 @@
+package com.gralliams.qrkash.viewmodel
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class WalletViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val sharedPreferences =
+        application.getSharedPreferences("WalletPrefs", Context.MODE_PRIVATE)
+    private val walletBalanceKey = "wallet_balance"
+
+    private val _walletBalance = MutableLiveData<Double>()
+    val walletBalance: LiveData<Double> get() = _walletBalance
+
+    private val _isBalanceSufficient = MutableLiveData<Boolean>()
+    val isBalanceSufficient: LiveData<Boolean> get() = _isBalanceSufficient
+
+    init {
+        _walletBalance.value = sharedPreferences.getFloat(walletBalanceKey, 100.0f).toDouble()
+        _isBalanceSufficient.value = true // Assume sufficient balance initially
+    }
+
+    fun decrementBalance(amount: Double) {
+        val currentBalance = _walletBalance.value ?: 0.0
+        if (amount <= currentBalance) {
+            _walletBalance.value = currentBalance - amount
+            saveBalanceToSharedPreferences()
+            _isBalanceSufficient.value = true // Sufficient balance
+        } else {
+            _isBalanceSufficient.value = false // Insufficient balance
+        }
+    }
+
+    fun incrementBalance(amount: Double) {
+        val currentBalance = _walletBalance.value ?: 0.0
+        _walletBalance.value = currentBalance + amount
+        saveBalanceToSharedPreferences()
+        _isBalanceSufficient.value = true // Reset to sufficient balance after incrementing
+    }
+
+    private fun saveBalanceToSharedPreferences() {
+        sharedPreferences.edit().putFloat(walletBalanceKey, _walletBalance.value!!.toFloat()).apply()
+    }
+}
--- functions/index.js
+++ functions/index.js
@@ -6,43 +6,62 @@ const db = admin.firestore();
 // Define your webhook endpoint
 exports.flutterwaveWebhook = functions.https.onRequest(async (req, res) => {
   try {
-//    // Validate the request
-//    const isValidRequest = validateRequest(req);
-//    if (!isValidRequest) {
-//      console.error('Invalid webhook request');
-//      return res.status(400).end();
-//    }
-
     // Parse the webhook payload
-    const { tx_ref, charged_amount, account_id } = req.body.data;
+    const { event, data } = req.body;
+    const { tx_ref, charged_amount, account_id } = data;
 
     // Get the user's wallet document
     const walletDoc = await db.collection('wallets').doc(account_id).get();
     if (!walletDoc.exists) {
       console.error('Wallet document not found');
-      return res.status(404).end();
+      return res.status(404).json({ error: 'Wallet document not found' });
     }
 
-    // Update the wallet balance
+    // Update the wallet balance based on the event type
     const walletData = walletDoc.data();
     const currentBalance = walletData.balance;
-    const updatedBalance = currentBalance + charged_amount;
 
-    // Update the wallet document in the database
-    await walletDoc.ref.update({ balance: updatedBalance });
+    // Handle different events that affect the wallet balance
+    switch (event) {
+      case 'charge.completed':
+        const updatedBalance = currentBalance + charged_amount;
+        await walletDoc.ref.update({ balance: updatedBalance });
+        break;
+
+      case 'charge.reversed':
+        // Parse the refunded amount from the event payload
+        const refundedAmount = data.refunded_amount || 0;
+
+        // Ensure the refunded amount does not exceed the balance
+        if (refundedAmount > currentBalance) {
+          console.error('Refunded amount exceeds wallet balance');
+          return res.status(400).json({ error: 'Refunded amount exceeds wallet balance' });
+        }
+
+        const newBalance = currentBalance - refundedAmount;
+        await walletDoc.ref.update({ balance: newBalance });
+        break;
+
+      // Add more cases for other relevant events if needed
+
+      default:
+        // For unhandled events, do nothing and log the event
+        console.error('Unhandled webhook event:', event);
+        return res.status(400).json({ error: 'Unhandled webhook event' });
+    }
 
     // Log the transaction in the wallet's transaction history
     const transactionHistoryRef = walletDoc.ref.collection('transactionHistory').doc();
     await transactionHistoryRef.set({
       tx_ref,
       amount: charged_amount,
-      timestamp: admin.firestore.FieldValue.serverTimestamp()
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
     });
 
     // Return a success response
-    return res.status(200).end();
+    return res.status(200).json({ message: 'Webhook processed successfully' });
   } catch (error) {
     console.error('Webhook processing error:', error);
-    return res.status(500).end();
+    return res.status(500).json({ error: 'Webhook processing error' });
   }
 });
