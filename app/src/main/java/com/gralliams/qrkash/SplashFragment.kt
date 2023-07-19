cb0569d484faf03681c504d18d1e3404ce4cdd69
Great Grant <greatgrant116@gmail.com>
2023-07-19 17:51:30 +0100

    Handle keep user logged in

--- app/src/main/java/com/gralliams/qrkash/SplashFragment.kt
+++ app/src/main/java/com/gralliams/qrkash/SplashFragment.kt
@@ -12,6 +12,7 @@ import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.gralliams.qrkash.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -35,10 +36,29 @@ class SplashFragment : Fragment() {
 
         // Use a handler to post a delayed action to open the main activity after the splash screen duration
         Handler(Looper.getMainLooper()).postDelayed({
-            findNavController().navigate(R.id.action_splashFragment_to_welcomeFragment)
+            var auth = FirebaseAuth.getInstance()
+
+            // Check if the user is signed in
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                // User is signed in, navigate directly to DashboardActivity
+                navigateToDashboard()
+            } else {
+                // User is not signed in, navigate to WelcomeFragment
+                findNavController().navigate(R.id.action_splashFragment_to_welcomeFragment)
+            }
+
         }, 1000)
 
         return binding.root
+    }
+
+    private fun navigateToDashboard() {
+        // Clear the activity stack and start DashboardActivity as a new task
+        val intent = Intent(requireContext(), DashboardActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        requireActivity().finish()
     }
 
 
