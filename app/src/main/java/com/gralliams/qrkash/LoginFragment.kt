0098e5e114ef0a0396f9b9f310ccafef987fcad2
Great Grant <greatgrant116@gmail.com>
2023-07-19 17:14:09 +0100

    validate login fields

--- app/src/main/java/com/gralliams/qrkash/LoginFragment.kt
+++ app/src/main/java/com/gralliams/qrkash/LoginFragment.kt
@@ -3,11 +3,13 @@ package com.gralliams.qrkash
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import androidx.fragment.app.Fragment
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
@@ -15,54 +17,70 @@ import com.gralliams.qrkash.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
-    // Initialize Firebase Authentication
     private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
-        val keepLoggedIn = getKeepLoggedInPreference()
-        if (keepLoggedIn) {
-            // Perform automatic login
-            startActivity(Intent(requireContext(), DashboardActivity::class.java))
-//            findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
-        }
-
-        binding.tvCreateAccount.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
-        }
         auth = FirebaseAuth.getInstance()
-        // Set up login button click listener
         binding.btnSignIn.setOnClickListener {
             login()
         }
+        binding.tvCreateAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+        }
 
+        setupInputValidation()
         return binding.root
     }
 
+    private fun setupInputValidation() {
+        binding.etEmail.doOnTextChanged { _, _, _, _ ->
+            binding.tilEmail.error = null
+        }
+
+        binding.etPassword.doOnTextChanged { _, _, _, _ ->
+            binding.tilPassword.error = null
+        }
+    }
+
     private fun login() {
-        val email = binding.etEmail.text.toString()
+        val email = binding.etEmail.text.toString().trim()
         val password = binding.etPassword.text.toString()
-        val keepLoggedIn = binding.checkboxKeepLoggedIn.isChecked
+
+        if (!validateEmail(email)) {
+            binding.tilEmail.error = "Invalid email address"
+            return
+        }
+
+        if (password.isEmpty()) {
+            binding.tilPassword.error = "Password is required"
+            return
+        }
 
         // Authenticate user with Firebase
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
+                    val keepLoggedIn = binding.checkboxKeepLoggedIn.isChecked
                     if (keepLoggedIn) {
-                        // Store user's preference to keep them logged in
                         saveKeepLoggedInPreference(true)
                     }
-                    // User login successful, handle success case
                     startActivity(Intent(requireContext(), DashboardActivity::class.java))
                 } else {
-                    // User login failed, handle failure case
                     Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        return if (email.isEmpty()) {
+            false
+        } else {
+            Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        }
     }
 
     private fun saveKeepLoggedInPreference(keepLoggedIn: Boolean) {
@@ -71,10 +89,5 @@ class LoginFragment : Fragment() {
             putBoolean("keep_logged_in", keepLoggedIn)
             apply()
         }
-    }
-
-    private fun getKeepLoggedInPreference(): Boolean {
-        val sharedPref = requireContext().getSharedPreferences("login_pref", Context.MODE_PRIVATE)
-        return sharedPref.getBoolean("keep_logged_in", false)
     }
 }
